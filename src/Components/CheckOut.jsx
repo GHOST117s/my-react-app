5dcadeeb5d301d214e103b7c3f0ba1fafc2f3ad8
GHOST117s <72849401+GHOST117s@users.noreply.github.com>
2023-01-29 21:40:48 +0530

    Update CheckOut.jsx

--- src/Components/CheckOut.jsx
+++ src/Components/CheckOut.jsx
@@ -20,7 +20,7 @@ const CheckOut = () => {
             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
           <h1 className="text-4xl font-bold">Thank You !</h1>
-          <p>Thank you for your interest! Check your email for a link to the guide.</p>
+          <p>Thank you for your interest! </p>
           <a
             className="inline-flex items-center px-4 py-2 text-white bg-indigo-600 border border-indigo-600 rounded rounded-full hover:bg-indigo-700 focus:outline-none focus:ring">
             <svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 mr-2" fill="none" viewBox="0 0 24 24"
