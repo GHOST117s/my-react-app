6a52ec9ebdd984b47560a4ed42607907a91a5a7c
GHOST117s <72849401+GHOST117s@users.noreply.github.com>
2023-01-29 21:44:40 +0530

    nothing changes

--- src/Components/CheckOut.jsx
+++ src/Components/CheckOut.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { Link,Navigate,Route,Routes,useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 const CheckOut = () => {
 
     const Navigate = useNavigate()
 
-    const handleCartButton =() =>{
+    const handlHomeButton =() =>{
         Navigate("/")
     }
 
@@ -20,14 +20,14 @@ const CheckOut = () => {
             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
           <h1 className="text-4xl font-bold">Thank You !</h1>
-          <p>Thank you for your interest! </p>
+          <p>Thank you for your interest!  </p>
           <a
             className="inline-flex items-center px-4 py-2 text-white bg-indigo-600 border border-indigo-600 rounded rounded-full hover:bg-indigo-700 focus:outline-none focus:ring">
             <svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 mr-2" fill="none" viewBox="0 0 24 24"
               stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M7 16l-4-4m0 0l4-4m-4 4h18" />
             </svg>
-            <span className="text-sm font-medium" onClick={handleCartButton}>
+            <span className="text-sm font-medium" onClick={handlHomeButton}>
               Home
             </span>
           </a>
